1a069567b5c7be1bfd81eb162452b149a10eb12b
Serhii Klymets <ksd2000@ukr.net>
2023-12-28 22:43:27 +0200

    Начальное состояние проекта
    удалено все лишнее, установлен пакет npm "modern-normalize"

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import css from './App.module.css';
+
+function App() {
+  return (
+    <div className={css['app-null']}>
+      <header>
+        react null
+      </header>
+    </div>
+  );
+}
+
+export default App;
